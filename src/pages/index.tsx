388fc97e46eb0ea98d9f43dd73c0f6972328a154
Jackson Santana <jack.rsantana@gmail.com>
2021-11-18 10:22:56 -0300

    Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss'
+
+// Client-side
+// Server-side
+// Static Side Generation
 
 interface HomeProps {
   product: {
@@ -38,7 +42,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1Jx9uLBQSxbArm4JxIsgU8Vm')
 
   const product = {
@@ -52,6 +56,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
